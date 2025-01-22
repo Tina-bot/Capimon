077f9314c4167ad2bf7af332a5c31279ded08f7f
DanielaMansilla1998 <dmansill@emeal.nttdata.com>
2025-01-22 16:23:11 -0300

    Feat: a Coach class was created with attributes and methods

--- src/main/java/org/example/model/Coach.java
+++ src/main/java/org/example/model/Coach.java
@@ -1,4 +1,31 @@
 package org.example.model;
 
+import java.util.Scanner;
+
 public class Coach {
+
+    // Atributos
+    private String name;
+
+    // Getters y setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    // Constructor
+    public Coach() {
+    }
+
+    // Métodos
+    public void setNickname(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese su nickname de entrenador: ");
+        this.setName(scanner.nextLine());
+        System.out.println("¡Bienvenido " + this.getName() + " a la Liga de los Capibara!");
+    }
 }
