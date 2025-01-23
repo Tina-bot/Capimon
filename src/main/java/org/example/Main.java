2482167448822d53f62c771ddfdf6b6029eec504
Alondra <alogadea@emeal.nttdata.com>
2025-01-23 16:04:28 -0300

    Add final boss

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,22 +8,22 @@ import org.example.service.utilities.CreateObjects;
 
 public class Main {
     public static void main(String[] args) {
-//        Coach coach = new Coach();
-//
-//        //Ingreso Nickname
-//        coach.setWelcomeMessage();
-//
-//        //Asignación de personaje al usuario con metodo random
-//        Capimon character = Coach.assignCharacter(coach);
-//        System.out.println(" ");
-//        System.out.println("Capibara asignado: " + character.getName());
-//        System.out.println(character.showInformation());
-//
-//
-//        //Incorporación de capibaras al usuario
-//        coach.setCapimonsUser(character);
-//        System.out.println("Capimones del usuario: " + coach.getCapimonsUser().toString());
-//        character.attack();
+            Coach coach = new Coach();
+
+     //Ingreso Nickname
+      coach.setWelcomeMessage();
+
+    //Asignación de personaje al usuario con metodo random
+        Capimon character = Coach.assignCharacter(coach);
+        System.out.println(" ");
+        System.out.println("Capibara asignado: " + character.getName());
+        System.out.println(character.showInformation());
+
+
+       //Incorporación de capibaras al usuario
+        coach.setCapimonsUser(character);
+      System.out.println("Capimones del usuario: " + coach.getCapimonsUser().toString());
+       character.attack();
 
         //Boss final
         FinalBoss finalBoss = new FinalBoss();
